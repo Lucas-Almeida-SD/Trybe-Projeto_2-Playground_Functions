09cc2f0620c3afac91f7b7ae1124fcc313aa7ec0
lucas_almeida <lucas.almdiniz@gmail.com>
2021-12-02 15:48:56 -0300

    Otimizaçao no requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -35,7 +35,7 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  let string = array[array.length - 1] + ', ' + array[0];
+  let string = `${array[array.length - 1]}, ${array[0]}`;
   return string;
 }
 
